f071eb726e2c5c92eb96399db68ff2de88b1e54b
robertay <94378311+robyuk@users.noreply.github.com>
2022-12-02 16:54:15 +0000

    Creates a QR code for each URL in a file - urls.txt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3
 """
-Creates a QR code in an image file
+Creates a QR code for each URL in a file - urls.txt
 
 Requires the pillow library, install like this:
 pip install pillow
@@ -8,51 +8,14 @@ pip install pillow
 
 import qrcode
 
-img = qrcode.make('https://www.raycomputing.com')
-img.save('qr.png')
+with open('urls.txt') as urlFile:
+  content = urlFile.read()
 
-# Test the QR code
-import cv2
-import webbrowser
-image=cv2.imread('qr.png')
-detector=cv2.QRCodeDetector()
-url, coords, pixels = detector.detectAndDecode(image)
+urls=content.split('\n')
 
-print(pixels, url, coords )
-webbrowser.open(url)
+for url in urls:
 
-'''
-""" To use an image captured from the camera, 
-copy the code below to a device with python3 and a camera:"""
+  img = qrcode.make(url)
+  filename=url.split('//')[1]
+  img.save(f'{filename}.png')
 
-#! /usr/bin/env python3
-"""
-Finds a QR code in an image captured from the camera and decodes it
-
-Requires CV2, install like this:
-pip install opencv-python
-"""
-
-import cv2
-import webbrowser
-
-video=cv2.VideoCapture(1)  # the integer may be 0 or 2 depending on your hardware configuration
-success,frame = video.read()
-detector=cv2.QRCodeDetector()
-
-while success:
-    url, coords, pixels = detector.detectAndDecode(frame)
-    if url:
-        webbrowser.open
-        break
-        
-    cv2.imshow('frame', frame)
-
-    if cv2.waitKey(1) == ord('q'):
-        break
-    success, frame = video.read()
-
-video.release()
-cv2.destroyAllWindows()
-
-'''
